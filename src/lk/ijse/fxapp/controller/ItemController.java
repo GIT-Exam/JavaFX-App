d9f4e33eb24d4150f92485f7207c76e66bdeb6d8
ThathsaraDV <thathsaradananjaya@gmail.com>
2021-10-09 10:48:37 +0530

    item model, controller and dbconnection added

---
+++ src/lk/ijse/fxapp/controller/ItemController.java
@@ -0,0 +1,8 @@
+package lk.ijse.fxapp.controller;
+
+/**
+ * @author Thathsara Dananjaya <thathsaradananjaya@gmail.com>
+ * @since 10/9/2021
+ **/
+public class ItemController {
+}
---
+++ src/lk/ijse/fxapp/db/DBConnection.java
@@ -0,0 +1,32 @@
+package lk.ijse.fxapp.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * @author Thathsara Dananjaya <thathsaradananjaya@gmail.com>
+ * @since 10/9/2021
+ **/
+public class DBConnection {
+
+    private static DBConnection dbConnection;
+    private Connection connection;
+
+    private DBConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/git","root","1234");
+    }
+
+    public static DBConnection getInstance() throws SQLException, ClassNotFoundException {
+        if (dbConnection == null){
+            dbConnection = new DBConnection();
+        }
+        return dbConnection;
+    }
+
+    public Connection getConnection(){
+        return connection;
+    }
+
+}
